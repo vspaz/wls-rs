728a353387a53863a0226c92039c7175d030fa6f
vspaz <vladimir.edelman@gmail.com>
2023-01-23 23:57:07 +0100

    add populate weights

--- src/models/wls.rs
+++ src/models/wls.rs
@@ -1,5 +1,11 @@
 pub struct Wls {
-    x: vec<f64>,
-    y: vec<f64>,
-    w: vec<f64>,
+    x: Vec<f64>,
+    y: Vec<f64>,
+    w: Vec<f64>,
+}
+
+impl Wls {
+    pub fn populate_weights(capacity: usize, value: f64) -> Vec<f64> {
+        return  vec![value; capacity];
+    }
 }
