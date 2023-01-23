bbbeed9362bb274ae645e67fa1e0291980285b56
vspaz <vladimir.edelman@gmail.com>
2023-01-23 23:41:06 +0100

    add constructor

--- src/models/point.rs
+++ src/models/point.rs
@@ -1,5 +1,10 @@
-
 pub struct Point {
-   intercept: f64,
-   slope: f64,
+    intercept: f64,
+    slope: f64,
+}
+
+impl Point {
+    pub fn new(intercept: f64, slope: f64) -> Point {
+        return Point { intercept, slope };
+    }
 }
