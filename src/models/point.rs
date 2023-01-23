9b8621410110b558979996a1064e9f737c8ef09c
vspaz <vladimir.edelman@gmail.com>
2023-01-23 23:43:42 +0100

    add get_intercept

--- src/models/point.rs
+++ src/models/point.rs
@@ -7,4 +7,8 @@ impl Point {
     pub fn new(intercept: f64, slope: f64) -> Point {
         return Point { intercept, slope };
     }
+
+    pub fn get_intercept(&self) -> f64 {
+        return self.intercept;
+    }
 }
