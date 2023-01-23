57ef200236e91ccdfaaeb358bdf7f2726e73faa5
vspaz <vladimir.edelman@gmail.com>
2023-01-24 00:07:06 +0100

    assert_have_size_greater_than_two

--- src/models/wls.rs
+++ src/models/wls.rs
@@ -5,11 +5,15 @@ pub struct Wls {
 }
 
 impl Wls {
-    fn populate_weights(capacity: usize, value: f64) -> Vec<f64> {
-        vec![value; capacity]
+    fn populate_weights(capacity: u64, value: f64) -> Vec<f64> {
+        vec![value; capacity as usize]
     }
 
-    fn assert_have_same_size(size_one: usize, size_two: usize) {
+    fn assert_have_same_size(size_one: u64, size_two: u64) {
         assert_eq!(size_one, size_two)
+    }
+
+    fn assert_have_size_greater_than_two(size_one: u64) {
+        assert!(size_one > 2)
     }
 }
