4df15f9f104a87b83b3b6f3d9792ce82e2156b39
vspaz <vladimir.edelman@gmail.com>
2023-01-26 00:29:47 +0100

    import optimizations

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod models;
 
+use crate::models::wls::Wls;
+
 fn main() {
     let x_points = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0];
     let y_points = vec![1.0, 3.0, 4.0, 5.0, 2.0, 3.0, 4.0];
     let weights = vec![10.0, 1.0, 3.0, 8.0, 14.0, 21.0, 13.0];
 
-    let wls = models::wls::Wls::new(x_points, y_points, Some(weights));
+    let wls = Wls::new(x_points, y_points, Some(weights));
     let point = wls.fit_linear_regression().unwrap();
 
     assert!(0.00000001 > 1.410964913 - point.get_intercept());
