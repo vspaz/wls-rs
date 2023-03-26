b8d25e3730918eeb32c09d2112685992eb3d53b2
vspaz <vladimir.edelman@gmail.com>
2023-03-26 12:28:52 +0200

    renam variables

--- src/asserts/asserts.rs
+++ src/asserts/asserts.rs
@@ -7,10 +7,10 @@ pub fn assert_true(condition: bool) {
     assert!(condition)
 }
 
-pub fn assert_have_same_size(one: &Vec<f64>, two: &Vec<f64>) {
-    assert_eq!(one.len(), two.len())
+pub fn assert_have_same_size(items_1: &Vec<f64>, items_2: &Vec<f64>) {
+    assert_eq!(items_1.len(), items_2.len())
 }
 
-pub fn assert_have_size_greater_than_two(one: &Vec<f64>) {
-    assert!(one.len() >= 2)
+pub fn assert_have_size_greater_than_two(items: &Vec<f64>) {
+    assert!(items.len() >= 2)
 }
